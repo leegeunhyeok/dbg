a6095ff6d96958b84ae186b90faba3d6f42219f9
leegeunhyeok <dev.ghlee@gmail.com>
2025-04-02 13:14:20 +0900

    test(runtime): update dbg-shim spec

--- src/runtime/dbg-shim.spec.ts
+++ src/runtime/dbg-shim.spec.ts
@@ -1,16 +1,31 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi, beforeEach, afterAll } from 'vitest';
 import { _ as dbg } from './dbg-shim';
 
 describe('dbg-shim', () => {
+  const consoleMock = vi
+    .spyOn(console, 'log')
+    .mockImplementation(() => undefined);
+
+  beforeEach(() => {
+    consoleMock.mockClear();
+  });
+
+  afterAll(() => {
+    consoleMock.mockReset();
+  });
+
   it('call with no arguments', () => {
     expect(dbg()).toBeUndefined();
+    expect(consoleMock).toHaveBeenCalledTimes(0);
   });
 
   it('call with one argument', () => {
     expect(dbg(1)).toBe(1);
+    expect(consoleMock).toHaveBeenCalledTimes(0);
   });
 
   it('call with multiple arguments', () => {
     expect(dbg(1, {}, 'foo')).toEqual([1, {}, 'foo']);
+    expect(consoleMock).toHaveBeenCalledTimes(0);
   });
 });
